5f1cb918b1911be43d69a2ec350d7727381e37ff
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-12-20 16:31:08 +0100

    Link donator to detente

--- resources/views/livewire/detente/individual.blade.php
+++ resources/views/livewire/detente/individual.blade.php
@@ -1,16 +1,16 @@
 @props(['detente'])
 <x-main-layout>
-    <h2>{{$detente->name}}</h2>
+    <h2 class="text-3xl">{{$detente->name}}</h2>
     <section class="py-4">
         <h3 class="text-2xl font-medium mb-4">Participants</h3>
         <ul class="grid grid-cols-3 gap-5">
-            @for($i = 0; $i < 9; $i++)
+            @foreach($detente->donators as $donator)
                 <li class="flex flex-col gap-1 px-5 py-3.5 relative rounded-lg bg-white">
-                    <p class="mb-0.5 font-medium">Antoine Copas</p>
-                    <a href="tel:0332425354" class="text-sm block">Tel : 0345 23 12 34</a>
-                    <a href="mailto:antoinecopas@gmail.com" class="text-sm block">Email : antoinecopas@gmail.be</a>
+                    <p class="mb-0.5 font-medium">{{ $donator->name }}</p>
+                    <a href="tel:{{ $donator->phone }}" class="text-sm block">Tel : {{ $donator->phone }}</a>
+                    <a href="mailto:{{ $donator->email }}" class="text-sm block">Email : {{ $donator->email }}</a>
                 </li>
-            @endfor
+            @endforeach
         </ul>
     </section>
     <section>
