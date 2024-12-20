81c5ae6157cc841f86a23ee315b77204d19699c4
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-12-20 16:28:08 +0100

    Fixing arrowBtn classes

--- app/View/Components/ArrowBtn.php
+++ app/View/Components/ArrowBtn.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class arrow-btn extends Component
+class ArrowBtn extends Component
 {
     /**
      * Create a new component instance.
